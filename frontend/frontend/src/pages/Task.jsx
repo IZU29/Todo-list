eb8fca653b995e5980d88904f2a87183942b8984
IZU29 <izunduche29@gmail.com>
2025-01-17 14:30:21 +0100

    More Changes Have Been Made

--- frontend/frontend/src/pages/Task.jsx
+++ frontend/frontend/src/pages/Task.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Task = ({task , setTask}) => {
   return (
-    <div className="border border-black w-3/4">{task.task}</div>
+    <div className="w-3/4 border border-green-500">
+    <div className="border border-black w-3/4">Id: {task._id}</div>
+    <div className="border border-black w-3/4">Task: {task.task}</div>
+    <div className="border border-black w-3/4">Status: {task.completed ? "Completed" : "Not-Done"}</div>
+    </div>
   )
 }
 
